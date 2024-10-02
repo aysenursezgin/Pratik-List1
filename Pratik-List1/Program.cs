470f7f9612657c44376f00e5011fa3c55cd64fca
aysenursezgin <115412288+aysenursezgin@users.noreply.github.com>
2024-10-02 09:16:34 +0300

    Proje dosyası ekle.

---
+++ Pratik-List1/Program.cs
@@ -0,0 +1,8 @@
+﻿List<string> Davetliler = new List<string>()
+{ "Bülent Ersoy","Ajda Pekkan","Ebru Gündeş","Hadise","Hande Yener","Tarkan","Funda Arar","Demet Akalın"};
+
+Console.WriteLine("Patika Plus Gala Gecesine Davetli Olanlar:");
+for (int i = 0; i < Davetliler.Count; i++)
+{
+    Console.WriteLine($"{i + 1}- {Davetliler[i]}");
+}
